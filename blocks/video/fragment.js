cff68e4cc2b12e1445450514b842b61dd62c5878
deepu25-eng <55683667+deepu25-eng@users.noreply.github.com>
2024-03-18 19:37:05 +0530

    Update video.js

--- blocks/video/fragment.js
+++ blocks/video/fragment.js
@@ -17,39 +17,15 @@ import {
  * @param {string} path The path to the fragment
  * @returns {HTMLElement} The root element of the fragment
  */
-export async function loadFragment(path) {
-  if (path && path.startsWith('/')) {
-    const resp = await fetch(`${path}.plain.html`);
-    if (resp.ok) {
-      const main = document.createElement('main');
-      main.innerHTML = await resp.text();
+const video = document.createElement('video');
 
-      // reset base path for media to fragment base
-      const resetAttributeBase = (tag, attr) => {
-        main.querySelectorAll(`${tag}[${attr}^="./media_"]`).forEach((elem) => {
-          elem[attr] = new URL(elem.getAttribute(attr), new URL(path, window.location)).href;
-        });
-      };
-      resetAttributeBase('img', 'src');
-      resetAttributeBase('source', 'srcset');
+video.muted = true; // exactly this way, otherwise it won't play in chrome. Thanks, @msagolj for the tip!
 
-      decorateMain(main);
-      await loadBlocks(main);
-      return main;
-    }
-  }
-  return null;
-}
-
-export default async function decorate(block) {
-  const link = block.querySelector('a');
-  const path = link ? link.getAttribute('href') : block.textContent.trim();
-  const fragment = await loadFragment(path);
-  if (fragment) {
-    const fragmentSection = fragment.querySelector(':scope .section');
-    if (fragmentSection) {
-      block.closest('.section').classList.add(...fragmentSection.classList);
-      block.closest('.fragment').replaceWith(...fragment.childNodes);
-    }
-  }
-}
+video.toggleAttribute('autoplay', true);
+video.toggleAttribute('loop', true);
+video.toggleAttribute('playsinline', true);
+const source = document.createElement('source');
+source.setAttribute('src', entry[2]);
+source.setAttribute('type', 'video/mp4');
+video.append(source);
+slide.append(video);
